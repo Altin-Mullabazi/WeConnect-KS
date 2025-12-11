f1fc4b21abb6c0e85c8481ab7d5c8ed9a6604fdf
Altinmullabazi <116796950+Altinmullabazi@users.noreply.github.com>
2025-12-11 23:50:37 +0100

    New Design

---
+++ script.js
@@ -0,0 +1,28 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+
+function showSlide(index) {
+  
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+
+   
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[currentSlide].classList.add('active');
+}
+
+
+function changeSlide(direction) {
+    showSlide(currentSlide + direction);
+}
+
+
+setInterval(() => {
+    changeSlide(1);
+}, 5000);
