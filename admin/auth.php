b6b9ab45baea5cba4937e6b2a2d105ee12b4b405
Altinmullabazi <116796950+Altinmullabazi@users.noreply.github.com>
2026-02-01 14:57:08 +0100

    Update dashboard

--- admin/auth.php
+++ admin/auth.php
@@ -1,19 +1,17 @@
 <?php
 session_start();
 
-// Kontrollo nëse përdoruesi është loguar
 if (!isset($_SESSION['user_id'])) {
     header('Location: ../login.php');
     exit;
 }
 
-// Kontrollo nëse përdoruesi është admin
 if (($_SESSION['role'] ?? '') !== 'admin') {
     header('Location: ../login.php');
     exit;
 }
 
-// Parandalon që faqja të ruhet në cache (butoni back nuk funksionon)
-header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1
-header("Pragma: no-cache"); // HTTP 1.0
-header("Expires: 0"); // Proxies
+
+header("Cache-Control: no-cache, no-store, must-revalidate"); 
+header("Pragma: no-cache"); 
+header("Expires: 0"); 
